05f29c53319b6e6cee36a92ba0f1eedf2be08ff7
Sam <nees@student.unimelb.edu.au>
2021-04-05 15:57:43 +1000

    restructured folders
    changed the folders so there is a clear distinction between the client and server, and added folders for customer and vendor routes so that we can make routes for each app that have the same app.js access point

--- server/app.js
+++ server/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const path = require("path");
 const exphbs = require("express-handlebars");
 const app = express();
 
 const port = process.env.PORT || 4000;
 
+app.set("views", path.join(__dirname, "../client/views"));
 app.engine(
   "hbs",
   exphbs({
-    defaultLayout: "main",
     extname: "hbs",
+    defaultLayout: "main",
   })
 );
-
 app.set("view engine", "hbs");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,6 +27,12 @@ app.get("/menu", (req, res) => {
 app.get("/food/:id", (req, res) => {
   res.render("showFood", { id: req.params.id });
 });
+
+// will need to do something like this
+// const customerRouter = require("./data/customer/routes/customer-router");
+// app.use("/customer", customerRouter)
+// and same for the vendor routes
+// and then make routes off /customer.. and /vendor..
 
 app.get("*", (req, res) => {
   res.render("home");

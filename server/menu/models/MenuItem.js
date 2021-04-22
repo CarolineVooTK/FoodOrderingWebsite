2b53f3090b1e254105e6be03a560f032ef33508f
sam-nee <nees@student.unimelb.edu.au>
2021-04-22 21:47:50 +1000

    remove ingredients from menuitem

--- server/menu/models/MenuItem.js
+++ server/menu/models/MenuItem.js
@@ -5,7 +5,6 @@ const MenuItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   photo: { type: String, required: true },
   price: { type: Number, required: true },
-  ingredients: { type: [String], required: true },
   description: { type: String, required: false },
 });
 

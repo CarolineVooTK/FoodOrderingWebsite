21d89d6a0242215d79699db219fbe31fb6ed628f
sam-nee <nees@student.unimelb.edu.au>
2021-05-06 22:10:56 +1000

    Update vendor menu page and navbar

--- frontend/public/js/helpers.js
+++ frontend/public/js/helpers.js
@@ -2,17 +2,42 @@ const myapi = require("../../routes/api");
 
 let register = (Handlebars) => {
   let helpers = {
-    listfood : function (data){
+    listfood: function (data) {
       var ret = "<ul>";
 
-      for (var i = 0, j = data.length; i < j; i ++){
-        ret = ret + "<li>"
-        "<img src ="+ data[i].photo +"</a>"
-        +"<a>"+ data[i].name +"</a>"
-        +"<a>"+ data[i].price + "</a>"
-        +"<a>"+ data[i].description + "</a>"
+      for (var i = 0, j = data.length; i < j; i++) {
+        ret = ret + "<li>";
+        "<img src =" +
+          data[i].photo +
+          "</a>" +
+          "<a>" +
+          data[i].name +
+          "</a>" +
+          "<a>" +
+          data[i].price +
+          "</a>" +
+          "<a>" +
+          data[i].description +
+          "</a>";
       }
-      return ret + "</ul>"
+      return ret + "</ul>";
+    },
+    vendorMenu: (data) => {
+      let menuitem = `<div class="row row-cols-3">`;
+      for (let i = 0; i < data.length; i++) {
+        menuitem += `<div class="row">
+            <h2 class="foodname" style="width: auto; float: left;">
+                <a href="/menu/${data[i]._id}">
+                    &nbsp;${data[i].name}
+                </a>
+            </h2>
+            <h2 style="width: auto; margin-left: auto; float: right;">${data[i].price}</h2>
+            <p style="clear: both;color:rgb(63, 63, 63)">&nbsp;${data[i].description}</p>
+            <img src="${data[i].photo}" width="280" height="280">
+            <button class="darkButton" onclick="">Add to Order</button>
+        </div>`;
+      }
+      return menuitem + `</div>`;
     },
   };
 

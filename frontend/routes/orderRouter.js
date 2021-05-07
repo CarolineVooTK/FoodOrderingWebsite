a3ba772e79f6f3075a21740a253832a9e2d1faa2
sam-nee <nees@student.unimelb.edu.au>
2021-05-07 23:27:57 +1000

    Add basic customer orders page

--- frontend/routes/orderRouter.js
+++ frontend/routes/orderRouter.js
@@ -14,27 +14,29 @@ const redirectToLogin = (req, res, next) => {
   }
 };
 
-orderRouter.get("/", async (req, res) => {
-    await myapi
-      .getOrders()
-      .then((data) => {
-        res.render("orders", { orders: data });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-  
-  
-  orderRouter.get("/:id",async (req, res) => {
-    await myapi
-      .getOrderDetails()
-      .then((data) => {
-        res.render("orders", { order: data[0] });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+// get all orders for a single customer
+orderRouter.get("/customer/:id", async (req, res) => {
+  await myapi
+    .getCustomerOrders({ id: req.params.id })
+    .then((data) => {
+      console.log(data);
+      res.render("orders", { orders: data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// get details for a single order
+orderRouter.get("/:id", async (req, res) => {
+  await myapi
+    .getOrderDetails({ id: req.params.id })
+    .then((data) => {
+      res.render("orders", { order: data[0] });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = orderRouter;

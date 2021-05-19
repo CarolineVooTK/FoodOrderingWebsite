70ba749f3e0932314ab00b258b839bf14c6bab23
JinYuanHee-Kane <jhhee@student.unimelb.edu.au>
2021-05-19 13:39:01 +0800

    Starting on displaying Outstanding Orders in a new page

--- vendor/routes/vendor-router.js
+++ vendor/routes/vendor-router.js
@@ -60,6 +60,7 @@ router.get("/:id", redirectToLogin, vendorController.getVendorById);
 router.post("/addVendor", vendorController.addNewVendor);
 // router.put("/:id/setVendorActive", vendorController.setVendorActive);
 router.get("/:id/outstandingOrders", vendorController.getOutstandingOrders);
+router.get("/getOutsOrdersByVendor", vendorController.getOutsOrdersByVendor); 
 
 
 module.exports = router;

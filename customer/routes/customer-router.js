1a465c1f8f4c0caa0c0a04bdb1b803718c5891fc
CarolineVooTK <61531436+CarolineVooTK@users.noreply.github.com>
2021-05-21 23:39:51 +0800

    fixed customer router

--- customer/routes/customer-router.js
+++ customer/routes/customer-router.js
@@ -41,9 +41,8 @@ router.get("/logout", (req, res, next) => {
   });
 });
 
-router.get(
-  "/profile", redirectToLogin,customerController.getCustDetails,
-  async (req, res, next) => {
+router.get("/profile", customerController.getCustDetails);
+router.get("/profile", redirectToLogin, async (req, res, next) => {
   if (req.session.orderlist && req.session.fromVendor ) {
     res.render("profile", { orderitems: req.session.orderlist, vendor: req.session.fromVendor});
   } else {

c49d37f19028560250ee5205008111b6e870b94d
chaisengl <chaisengl@student.unimelb.edu.au>
2021-05-30 04:06:36 +1000

    added unit test setVendorOff

--- __tests__/unitTests.js
+++ __tests__/unitTests.js
@@ -217,3 +217,116 @@ describe("Unit testing setVendorActive vendorController.js with valid informatio
           });
     });
   });
+
+describe("Unit testing setVendorOff vendorController.js with valid information", () => {
+  const req = {
+      // assuming the vendor is logined with session.passport.user = "6075878024b5d615b324ee1d"
+      session:{passport:{user:"6075878024b5d615b324ee1d"}},
+  };
+
+  // response object with render and status
+  // in vendorController it is called by res.status(200).render
+  const res = {
+      render: jest.fn(),
+      status: jest.fn(() => res)
+  }; 
+
+  beforeAll(() => {
+      res.render.mockClear();
+      // mock data for vendor
+      vendors.findById = jest.fn().mockResolvedValue([{
+          orders: [ "607b912c24b5d615b324ee2b" ],
+          _id: "6075878024b5d615b324ee1d",
+          name: 'Vendor 1',
+          email: 'vendor@test.com',
+          location: { coordinates: [ 99, 99 ], type: 'Point' },
+          active: true,
+          menu: [
+            { menuitem: "607a431a24b5d615b324ee1e", quantity: 20 },
+            { menuitem: "607b75ab24b5d615b324ee27", quantity: 10 },
+            { menuitem: "60815f9824b5d615b324ee3c", quantity: 20 },
+            { menuitem: "60815ff324b5d615b324ee3d", quantity: 10 },
+            { menuitem: "6081605b24b5d615b324ee3e", quantity: 20 },
+            { menuitem: "608160c024b5d615b324ee3f", quantity: 10 },
+            { menuitem: "6081614c24b5d615b324ee40", quantity: 20 },
+            { menuitem: "608161df24b5d615b324ee41", quantity: 10 }
+          ],
+          password: '$2a$10$3rqBgzXZaPQsQyTSZ0Yj1.L1g.CDbarI2m1L1Pp2Hn9M9/zstjUJK',
+          textlocation: 'jest test location'
+        }])
+
+      // mock rating for the vendor
+      orders.aggregate = jest.fn().mockResolvedValue([{
+          _id: null, rating: 2, count: 2
+      }])
+
+      vendors.findOneAndUpdate = jest.fn().mockResolvedValue([{
+          orders: [ "607b912c24b5d615b324ee2b" ],
+          _id: "6075878024b5d615b324ee1d",
+          name: 'Vendor 1',
+          email: 'vendor@test.com',
+          location: { coordinates: [ 99, 99 ], type: 'Point' },
+          active: true,
+          menu: [
+            { menuitem: "607a431a24b5d615b324ee1e", quantity: 20 },
+            { menuitem: "607b75ab24b5d615b324ee27", quantity: 10 },
+            { menuitem: "60815f9824b5d615b324ee3c", quantity: 20 },
+            { menuitem: "60815ff324b5d615b324ee3d", quantity: 10 },
+            { menuitem: "6081605b24b5d615b324ee3e", quantity: 20 },
+            { menuitem: "608160c024b5d615b324ee3f", quantity: 10 },
+            { menuitem: "6081614c24b5d615b324ee40", quantity: 20 },
+            { menuitem: "608161df24b5d615b324ee41", quantity: 10 }
+          ],
+          password: '$2a$10$3rqBgzXZaPQsQyTSZ0Yj1.L1g.CDbarI2m1L1Pp2Hn9M9/zstjUJK',
+          textlocation: 'jest test location'
+        }])
+        
+      vendors.findById.mockImplementationOnce(()=> ({
+          lean: jest.fn().mockReturnValue({
+              orders: [ "607b912c24b5d615b324ee2b" ],
+              _id: "6075878024b5d615b324ee1d",
+              name: 'Vendor 1',
+              email: 'vendor@test.com',
+              location: { coordinates: [ 99, 99 ], type: 'Point' },
+              active: true,
+              menu: [
+                { menuitem: "607a431a24b5d615b324ee1e", quantity: 20 },
+                { menuitem: "607b75ab24b5d615b324ee27", quantity: 10 },
+                { menuitem: "60815f9824b5d615b324ee3c", quantity: 20 },
+                { menuitem: "60815ff324b5d615b324ee3d", quantity: 10 },
+                { menuitem: "6081605b24b5d615b324ee3e", quantity: 20 },
+                { menuitem: "608160c024b5d615b324ee3f", quantity: 10 },
+                { menuitem: "6081614c24b5d615b324ee40", quantity: 20 },
+                { menuitem: "608161df24b5d615b324ee41", quantity: 10 }
+              ],
+              password: '$2a$10$3rqBgzXZaPQsQyTSZ0Yj1.L1g.CDbarI2m1L1Pp2Hn9M9/zstjUJK',
+              textlocation: 'jest test location'
+          }),
+          }));
+      vendorController.setVendorOff(req,res)
+    });
+    test("Test case 1: using valid vendor id with valid location to set active", () => {    
+      expect(res.render ).toHaveBeenCalledTimes(1);
+      expect(res.render).toHaveBeenCalledWith("vendorProfile", {
+          vendor_status: "Off", vendor: {
+              orders: [ "607b912c24b5d615b324ee2b" ],
+              _id: "6075878024b5d615b324ee1d",
+              name: 'Vendor 1',
+              email: 'vendor@test.com',
+              location: { coordinates: [ 99, 99 ], type: 'Point' },
+              active: true,
+              menu: [
+                { menuitem: "607a431a24b5d615b324ee1e", quantity: 20 },
+                { menuitem: "607b75ab24b5d615b324ee27", quantity: 10 },
+                { menuitem: "60815f9824b5d615b324ee3c", quantity: 20 },
+                { menuitem: "60815ff324b5d615b324ee3d", quantity: 10 },
+                { menuitem: "6081605b24b5d615b324ee3e", quantity: 20 },
+                { menuitem: "608160c024b5d615b324ee3f", quantity: 10 },
+                { menuitem: "6081614c24b5d615b324ee40", quantity: 20 },
+                { menuitem: "608161df24b5d615b324ee41", quantity: 10 }
+              ],
+              password: '$2a$10$3rqBgzXZaPQsQyTSZ0Yj1.L1g.CDbarI2m1L1Pp2Hn9M9/zstjUJK',
+              textlocation: 'jest test location'
+          }, rating: 2, count: 2,});
+  });
+});
